2c6998b99f957f04d83345bd8934bad0e9aa6514
aurgenblick <aurgenblick@gmail.com>
2022-09-18 18:54:28 +0300

    Added explaining comments on methods dispatch

--- src/Q3.java
+++ src/Q3.java
@@ -7,26 +7,47 @@ class Q3 {
     E e2 = new F(); // reference type E, upcasting to F
     F f1 = new F(); // reference type F, no class casting
 
-    d1.m(e2); //*reference type is D, object type is D, parameter reference type is E, parameter object type is F,
-              // therefore, the method is chosen amongst class D methods . Static binding will choose m(E),
-              // since the parameter reference type is E and class В has method m(E e).
+    d1.m(e2); //*Compilation :
+              // calling object declared type is D, parameter declared type is E
+              // -> compiler will search for the family of methods as m(E) .
+              // Runtime :
+              // calling object real type is D(, passed object reals type is F),
+              // -> the method m(E) is first searched amongst class D methods and will choose the existing one.
               // DE
-    e1.m(d3); //*reference type is E, object type is E, parameter reference type is D, parameter object type is F,
-              // therefore, at first, the method is searched for amongst class E, where there is method m(D d) .
+    e1.m(d3); //*Compilation :
+              // calling object declared type is E, parameter declared type is D
+              // -> compiler will search for the family of methods as m(D) .
+              // Runtime :
+              // calling object real type is E(, passed object reals type is F),
+              // -> the method m(D) is first searched amongst class E methods and the existing one will be chosen .
               // ED
-    e2.m(f1); //*reference type is E, object type is F, parameter reference type is F, parameter object type is F,
-              // therefore, at first, the method is searched for amongst class F, where there is method m(F f) .
+    e2.m(f1); //*Compilation :
+              // calling object declared type is E, parameter declared type is F
+              // -> compiler will search for the family of methods as m(F) .
+              // Runtime :
+              // calling object real type is F(, passed object reals type is F),
+              // -> the method m(F) is first searched amongst class F methods and the existing one will be chosen .
               // FF
-    f1.m(e2); //*reference type is F, object type is F, parameter reference type is E, parameter object type is F,
-              // therefore, at first, the method is searched for amongst class F, where there is method m(F f),
-              // but since the parameter reference type is E(and class F doesn't have m(E e)),
-              // method is then searched for in class E which has m(E e).
+    f1.m(e2); //*Compilation :
+              // calling object declared type is F, parameter declared type is E
+              // -> compiler will search for the family of methods as m(E) .
+              // Runtime :
+              // calling object real type is F(, passed object reals type is F),
+              // -> the method m(E) is first searched amongst class F methods,
+              // but since there is no such method in F class, the search will be continued in F ancestor class E and
+              // method m(E) of class E will be chosen .
               // EE
-    d2.m(d3); //*reference type is D, object type is E, parameter reference type is D, parameter object type is F,
-              // therefore, at first, the method is searched for amongst class E, where there is method m(F f),
-              // but since the parameter reference type is D(and class E doesn't have m(E e)),
-              // method is then searched for in class E which has m(E e).
-              // EE
+    d2.m(d3); //*Compilation :
+              // calling object declared type is D, parameter declared type is D
+              // -> compiler will search for the family of methods as m(D) .
+              // Runtime :
+              // calling object real type is E(, passed object reals type is F),
+              // -> the method m(D) is first searched amongst class E methods,
+              // but since there is no such method in E class, the search will be continued in E ancestor class D .
+              // However, class D also doesn't have method m(D), and
+              // -> the signature of method m() will be upcasted to Object, and the sought-for method will be m(Object),
+              // -> method m(Object) of class D will be chosen .
+              // DO
   }
 }
 
